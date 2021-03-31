0cf06f24e77888a7ed336272522f87f3c0064819
mario-sanz <mariosg2001@gmail.com>
2021-03-31 12:06:34 +0200

    Somos retrasados

--- src/simulator/control/DifferentStatesException.java
+++ src/simulator/control/DifferentStatesException.java
@@ -6,7 +6,7 @@ public class DifferentStatesException extends Exception {
 
     public DifferentStatesException(JSONObject expState, JSONObject currState, int step) {
         super("Expected state: " + expState.toString() + "\n"
-                + "Current state: " + expState.toString() + "\n"
+                + "Current state: " + currState.toString() + "\n"
                 + "Execution step number: " + Integer.toString(step));
     }
 }
