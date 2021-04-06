b8731622d2356c08ab30a6a67b4319e20da15935
mario-sanz <mariosg2001@gmail.com>
2021-04-06 10:24:15 +0200

    Outputs included and last changes

--- src/simulator/control/Controller.java
+++ src/simulator/control/Controller.java
@@ -35,7 +35,6 @@ public class Controller {
         }
     }
 
-    // TODO: REVISAR SI NOS ***SOBRA*** ALGO (MY BROUSTON KNOWS WHAT I MEAN)
     public void run(int steps, OutputStream out, InputStream expOut, StateComparator cmp) throws DifferentStatesException {
         JSONObject expOutJO = null;
         if (expOut != null) {
