042ad2f91a9769c244fd46a8febe5f6ec998d72a
mario-sanz <mariosg2001@gmail.com>
2021-04-22 11:31:17 +0200

    StatusBar finished
    Co-authored-by: Nesmeg <nesmeg@users.noreply.github.com>

--- src/simulator/view/StatusBar.java
+++ src/simulator/view/StatusBar.java
@@ -25,41 +25,61 @@ public class StatusBar extends JPanel implements SimulatorObserver {
     private void initGUI() {
         this.setLayout( new FlowLayout( FlowLayout.LEFT ));
         this.setBorder( BorderFactory.createBevelBorder( 1 ));
-        // TODO complete the code to build the tool bar
+        
+        _currTime = new JLabel();
+        _currLaws = new JLabel();
+        _numOfBodies = new JLabel();
     }
 
-    // other private/protected methods
-    // ...
+    private void updateStatusBar() {
+        this.add(_currTime);
+		this.add(_numOfBodies);
+		this.add(_currLaws);
+    }
 
     // SimulatorObserver methods
     // ...
     @Override
     public void onRegister(List<Body> bodies, double time, double dt, String fLawsDesc) {
-
+        _currLaws.setText("Laws: " + fLawsDesc);
+        _currTime.setText("Time: " + Double.toString(time));
+        _numOfBodies.setText("Bodies: " + Integer.toString(bodies.size()));
+        
+        updateStatusBar();
     }
 
     @Override
     public void onReset(List<Body> bodies, double time, double dt, String fLawsDesc) {
+        _currLaws.setText("Laws: " + fLawsDesc);
+        _currTime.setText("Time: " + Double.toString(time));
+        _numOfBodies.setText("Bodies: " + Integer.toString(bodies.size()));
 
+        updateStatusBar();
     }
     
     @Override
     public void onBodyAdded(List<Body> bodies, Body b) {
+        _numOfBodies.setText("Bodies: " + Integer.toString(bodies.size()));
 
+        updateStatusBar();
     }
     
     @Override
     public void onAdvance(List<Body> bodies, double time) {
+        _currTime.setText("Time: " + Double.toString(time));
 
+        updateStatusBar();
     }
     
     @Override
     public void onDeltaTimeChanged(double dt) {
-
+        updateStatusBar();
     }
     
     @Override
     public void onForceLawsChanged(String fLawsDesc) {
+        _currLaws.setText("Laws: " + fLawsDesc);
 
+        updateStatusBar();
     }
 }
